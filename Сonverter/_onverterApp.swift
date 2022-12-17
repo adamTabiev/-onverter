31262f98a669837b6e9f428c093a2af3e34f0c5a
adamTab <tabievadam@gmail.com>
2022-12-17 18:15:18 +0100

    Initial Commit

---
+++ Сonverter/_onverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _onverterApp.swift
+//  Сonverter
+//
+//  Created by Адам Табиев on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct _onverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
